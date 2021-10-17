0504538c8809e350a4aebee9152f86e9c7e79ba7
Josué <vintetrezi@gmail.com>
2021-10-17 17:23:32 -0300

    Update on the login screen.

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.infox.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author jogul
+ */
+public class ModuloConexao {
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        String driver = "com.msql.jdbc.Driver";
+    }
+}
